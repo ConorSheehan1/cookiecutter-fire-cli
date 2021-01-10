0c75a642d7b9469e982cd48f10bb9fcb3ab76ae8
Conor Sheehan <conor.sheehan.dev@gmail.com>
2021-01-10 19:09:47 +0000

    conditionally add release github action

---
+++ hooks/README.md
@@ -0,0 +1,4 @@
+Hooks for cookiecutter.
+
+Currently used to remove unwanted github actions.
+https://github.com/cookiecutter/cookiecutter/issues/723#issuecomment-350561930
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,19 @@
 import os
 import shutil
 
+
 def remove_generated_file(filepath):
+    # hooks are run from generated dir context, so will be ./example_cli
     full_path = os.path.join(os.getcwd(), '{{cookiecutter.repo_name}}', filepath)
     if os.path.isfile(full_path):
         os.remove(filepath)
 
-create_pypi_deploy_actions = '{{cookiecutter.create_pypi_deploy_github_action}}' == 'y'
 
-if not create_pypi_deploy_actions:
-    remove_generated_file(os.path.join('.github', 'workflows', 'deploy'))
+create_pypi_deploy_action = '{{cookiecutter.create_pypi_deploy_github_action}}' == 'y'
+create_release_action = '{{cookiecutter.create_release_github_action}}' == 'y'
+
+if not create_pypi_deploy_action:
+    remove_generated_file(os.path.join('.github', 'workflows', 'deploy.yml'))
+
+if not create_release_action:
+    remove_generated_file(os.path.join('.github', 'workflows', 'release.yml'))
